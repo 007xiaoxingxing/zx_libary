b2b89ed209df0b26d38a48c42758a797b1ddcd2f
007xiaoxingxing <chanxing9@gmail.com>
2016-12-22 20:49:46 +0800

    a

--- MsgService.py
+++ MsgService.py
@@ -6,8 +6,8 @@ class MsgService:
         toUser = msgDict['fromUser']
         fromUser = msgDict['toUser']
         if msgDict['msgType'] == 'text' and msgDict['msgContent'] == "借书":
-            return TextMsg(toUser,fromUser,u"有人借书拉".encode('utf-8')).format()
+            return TextMsg(toUser,fromUser,"有人借书拉").format()
         else:
             print msgDict['msgContent']
-            return TextMsg(toUser,fromUser,msgDict['msgContent']).format()
+            return TextMsg(toUser,fromUser,"回复中文").format()
 
