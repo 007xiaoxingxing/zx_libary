82fef8ad188b408a9605c10701d208effb8ebe75
007xiaoxingxing <chanxing9@gmail.com>
2016-12-22 18:41:12 +0800

    init

---
+++ Main.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+import tornado.web
+import tornado.ioloop
+import hashlib
+from WechatAPI import *
+
+class MainHandler(tornado.web.RequestHandler):
+    def post(self):
+        wechatXML = self.request.body
+
+        self.write("aaa")
+    #处理来自微信服务器的get请求，即第一次的认证请求
+    def get(self):
+        signature = self.get_argument('signature')
+        timestamp = self.get_argument('timestamp')
+        nonce = self.get_argument('nonce')
+        echostr = self.get_argument('echostr')
+        token = "xingxing"
+
+        list = [token, timestamp, nonce]
+        list.sort()
+        sha1 = hashlib.sha1()
+        map(sha1.update, list)
+        hashcode = sha1.hexdigest()
+        if hashcode == signature:
+            self.write(echostr)
+def main_app():
+    return tornado.web.Application([
+        (r'/',MainHandler),
+    ])
+if __name__ == "__main__":
+    app = main_app()
+    app.listen(81)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ WechatAPI.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+import xml.etree.cElementTree as ET
+class WechatAPI:
+    def __init__(self):
+        print "init"
+
+    #解析微信服务器更传回的xml
+    def ParseWechatXML(self,wechatXML):
+        root = ET.fromstring(wechatXML)
+        fromUser = root.find('FromUserName').text
+        toUser = root.find('ToUserName').text
+        createTime = root.find('CreateTime').text
+        msgId = root.find('MsgId').text
+        msgType = root.find('MsgType').text
+        result = {"fromUser":fromUser,"toUser":toUser,"createTime":createTime,"msgId":msgId,"msgType":msgType}
+        #拼装文本消息
+        if msgType == 'text':
+            msgContent = root.find('Content').text
+            result["msgContent"] = msgContent
+            return result
+
