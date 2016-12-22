9bbf66bcd8c800a76ef47a7d1749f9714f9bf448
007xiaoxingxing <chanxing9@gmail.com>
2016-12-22 21:21:32 +0800

    a

--- MsgService.py
+++ MsgService.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8 -*-
 from reply import *
+import urllib2
+import urllib
+import json
 class MsgService:
     #获取消息路由，确定由哪个具体的方法来处理消息，也就是设置关键字
     def GetMsgRouter(self,msgDict):
@@ -7,7 +10,14 @@ class MsgService:
         fromUser = msgDict['toUser']
         if msgDict['msgType'] == 'text' and msgDict['msgContent'] == "借书":
             return TextMsg(toUser,fromUser,"有人借书拉").format()
+        elif msgDict['msgType'] == 'text' and msgDict['msgContent'] == "还书":
+            return TextMsg(toUser,fromUser,"有人还书拉").format()
+        if msgDict['msgType'] == 'text' and msgDict['msgContent'] == "绑定":
+            return TextMsg(toUser,fromUser,"有人要绑定拉").format()
         else:
-            print msgDict['msgContent']
-            return TextMsg(toUser,fromUser,"回复中文").format()
+            tuling_url = "http://www.tuling123.com/openapi/api"
+            key = "ad4c133e35c744acb3c707bd27d74e87"
+            data = {'key':key,'info':msgDict['msgContent']}
+            res = urllib2.urlopen(tuling_url,urllib.urlencode(data)).read()
+            return TextMsg(toUser,fromUser,json.loads(res)['text']).format()
 
--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 # -*- coding:utf-8 -*-
-from reply import *
-t=TextMsg('a','a','中文aaaa哈斯hi')
-print t.format()
+import urllib2
+import urllib
+import json
+tuling_url = "http://www.tuling123.com/openapi/api"
+key = "ad4c133e35c744acb3c707bd27d74e87"
+data = {'key':key,'info':'我是谁'}
+res = urllib2.urlopen(tuling_url,urllib.urlencode(data)).read()
+print json.loads(res)['text']
