d560fb4cacf490e4fccee9e0c41b7ee225573b4b
007xiaoxingxing <chanxing9@gmail.com>
2016-12-28 22:44:42 +0800

    check

--- MsgService.py
+++ MsgService.py
@@ -13,8 +13,8 @@ class MsgService:
             return ArticalMsg(toUser,fromUser,'欢迎来到呼呼借书，呼呼队长正在待命。1、2、3','既然你诚心诚意的关注了、那我们就大发慈悲地告诉你、为了防止世界被破坏、为了维护宇宙的和平、惯彻爱与真实的邪恶、我们是穿梭在知乎的借书队、好看、经典的图书等待着你们、就是这样、喵','http://lib.star-chen.com/sub?openid=').format()
         if msgDict['msgType'] == 'text' and msgDict['msgContent'] == "借书":
             return ArticalMsg(toUser,fromUser,'哟？你要借书吗?','快点我，点我，我带你飞!','http://lib.star-chen.com/borrow?openid=').format()
-        #elif msgDict['msgType'] == 'text' and msgDict['msgContent'] == "还书":
-           # return TextMsg(toUser,fromUser,"有人还书拉").format()
+        elif msgDict['msgType'] == 'text' and msgDict['msgContent'] == "还书":
+            return ArticalMsg(toUser,fromUser,'哟？你要还书吗?','快点我，点我，我带你去还书!','http://lib.star-chen.com/person?openid=').format()
         elif msgDict['msgType'] == 'text' and msgDict['msgContent'] == "绑定":
             return ArticalMsg(toUser,fromUser,'点我进入绑定流程~','霞姐说了,如果你不绑定是没法借书滴','http://lib.star-chen.com/bind?openid=').format()
         elif msgDict['msgType'] == 'text' and msgDict['msgContent'] == "审核" and msgDict['fromUser'] in ['oy1IAs2qSA2K9Ilx0UBoe117V6XI','oy1IAs2qSA2K9Ilx0UBoe117V6XI','oszGFwTFy5WroZkXMPU7sVgwsw_Y']:
